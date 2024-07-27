84d5fc5c2f11d591c1045d711bb23a2f94a2ad52
Jairo Antonio Camarillo Tinoco <jcamarillo@lumston.com>
2024-07-27 10:14:03 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Straapberry',
+  webDir: 'www'
+};
+
+export default config;
