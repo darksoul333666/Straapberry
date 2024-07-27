fa600860be63d8860420893e50f6e3627331dd97
Jairo Antonio Camarillo Tinoco <jcamarillo@lumston.com>
2024-07-27 14:40:06 -0600

    feat: adding storage and functions to handle auth, login, and register of users

--- src/app/core/auth/auth.module.ts
+++ src/app/core/auth/auth.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthenticationRoutingModule } from './auth-routing.module';
-
-
+import { IonicStorageModule } from '@ionic/storage-angular';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    AuthenticationRoutingModule
+    AuthenticationRoutingModule,
+    IonicStorageModule.forRoot({
+      name: '__mydb',
+      driverOrder: ['sqlite', 'indexeddb', 'websql']
+    })
   ]
 })
 export class AuthModule { }
