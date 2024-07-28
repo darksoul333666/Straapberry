ff33cfeb3b79ede2d4e2526cb7d2da2ddef4f290
Jairo Antonio Camarillo Tinoco <jcamarillo@lumston.com>
2024-07-27 21:54:04 -0600

    feat: logic for register and login

--- src/app/core/auth/authentication-login/authentication-login.component.ts
+++ src/app/core/auth/authentication-login/authentication-login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-authentication-login',
@@ -6,9 +8,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./authentication-login.component.scss'],
 })
 export class AuthenticationLoginComponent  implements OnInit {
+  loginForm: FormGroup = new FormGroup({});
+  public loading = false;
+  constructor(
+    public formBuilder: FormBuilder,
+    private readonly authService: AuthService
+  ) { }
 
-  constructor() { }
+  public ngOnInit() {
+    this.initForm();
+  }
 
-  ngOnInit() {}
+  /**
+   * Initialize login form, with email and password
+   * @returns void
+   */
+  public initForm(): void {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['']
+    })
+  }
 
+  /**
+   * Login user using email and password, calling login method of AuthService
+   * @returns void
+   */
+  public async onLoginClick(): Promise<void> {
+    this.loading = true;
+    try {
+      const user = await this.authService.login(this.loginForm.value);
+      this.loading = false;
+    } catch (error) {
+      this.loading = false;
+    }
+  }
+
+  /**
+   * Check if passwords match, returning boolean
+   * @returns boolean
+   */
+  public passwordsMatch(): boolean {
+    return this.loginForm.get('password')?.value === this.loginForm.get('confirmPassword')?.value
+  }
+
+  /**
+   * LifeCycle hook, ionViewWillLeave, reset login form, set loading to false
+   * @returns void
+   */
+  public ionViewWillLeave(): void {
+    this.loading = false;
+    this.loginForm.reset();
+  }
 }
--- src/app/core/auth/authentication-register/authentication-register.component.ts
+++ src/app/core/auth/authentication-register/authentication-register.component.ts
@@ -1,14 +1,65 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-authentication-register',
   templateUrl: './authentication-register.component.html',
   styleUrls: ['./authentication-register.component.scss'],
 })
-export class AuthenticationRegisterComponent  implements OnInit {
+export class AuthenticationRegisterComponent  implements OnInit{
+  registerForm: FormGroup = new FormGroup({});
+  public loading = false;
+  constructor(
+    private readonly formBuilder: FormBuilder,
+    private readonly authService: AuthService
+  ) { }
 
-  constructor() { }
+  public ngOnInit() {
+    this.initForm();
+  }
 
-  ngOnInit() {}
+  /**
+   * Initialize register form, with email and password
+   * @returns void
+   */
+  public initForm(): void {
+    this.registerForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: [null, [Validators.required]],
+      confirmPassword: [null, [Validators.required]],
+      name: [null, [Validators.required]]
+    })
+  }
+
+  /**
+   * Function called when register button is clicked
+   * @returns void
+   * @throws Error if email already exists
+   */
+  public async onRegisterClick(): Promise<void> {
+    try {
+      this.authService.addUser(this.registerForm.value)
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  /**
+   * Function to check if passwords match
+   * @returns boolean
+   */
+  public passwordsMatch(): boolean {
+    return this.registerForm.get('password')?.value === this.registerForm.get('confirmPassword')?.value
+  }
+
+  /**
+   * LifeCycle hook, ionViewWillLeave, reset register form, set loading to false
+   * @returns void
+   */
+  public ionViewWillLeave(): void {
+    this.loading = false;
+    this.registerForm.reset();
+  }
 
 }
