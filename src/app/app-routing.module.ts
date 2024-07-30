c510a2f5b6dff73407f64d602dadf3582ccfcdd7
Jairo Antonio Camarillo Tinoco <jcamarillo@lumston.com>
2024-07-30 02:08:55 -0600

    feat: fixing compilation for android

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { ROUTES } from './shared/constants/routes';
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo:  ROUTES.LOGIN,
+    pathMatch: 'full',
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./core/auth/auth.module').then((m) => m.AuthModule),
+  },
+  {
+    path: 'admin',
+    loadChildren: () => import('./admin/admin.module').then((m) => m.AdminModule),
+  },
+  {
+    path: 'client',
+    loadChildren: () => import('./client/client.module').then((m) => m.ClientModule),
+  }
+];
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
